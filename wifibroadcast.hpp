f861adcd86c985804137a916b0ac6cf1a8512708
Vasily Evseenko <svpcom@gmail.com>
2017-05-17 08:34:01 +0300

    Fix compat with old g++ versions

--- wifibroadcast.hpp
+++ wifibroadcast.hpp
@@ -40,11 +40,11 @@
 using namespace std;
 
 template<typename ... Args>
-string string_format( const std::string& format, Args ... args )
+string string_format( const char *format, Args ... args )
 {
-    size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+    size_t size = snprintf(nullptr, 0, format, args ...) + 1; // Extra space for '\0'
     unique_ptr<char[]> buf(new char[ size ]);
-    snprintf(buf.get(), size, format.c_str(), args ...);
+    snprintf(buf.get(), size, format, args ...);
     return string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 }
 
