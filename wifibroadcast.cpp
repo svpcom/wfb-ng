86de92babdc8d083dff098efb6fffd01d86e631c
Vasily Evseenko <svpcom@gmail.com>
2018-11-15 09:23:48 +0300

    Rewrite string_format function #22

--- wifibroadcast.cpp
+++ wifibroadcast.cpp
@@ -15,8 +15,11 @@ string string_format(const char *format, ...)
     va_list args;
     va_start(args, format);
     size_t size = vsnprintf(nullptr, 0, format, args) + 1; // Extra space for '\0'
-    unique_ptr<char[]> buf(new char[ size ]);
+    va_end(args);
+    unique_ptr<char[]> buf(new char[size]);
+    va_start(args, format);
     vsnprintf(buf.get(), size, format, args);
+    va_end(args);
     return string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 }
 
