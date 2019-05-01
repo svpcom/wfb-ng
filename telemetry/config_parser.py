5135695a14df0cdfbfacc8a3898e29702fae4e5a
Vasily Evseenko <svpcom@gmail.com>
2019-05-01 12:52:27 +0300

    Code cleanup

--- telemetry/config_parser.py
+++ telemetry/config_parser.py
@@ -22,12 +22,11 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from builtins import *
+
 from future import standard_library
 standard_library.install_aliases()
 
-from builtins import str
-from builtins import object
+from builtins import *
 
 import configparser
 import ast
