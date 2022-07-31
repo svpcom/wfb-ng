0f6fc3d1dc73a269b09c1b7da766e0f0397b660e
Leon Jungemeyer <leon.jungemeyer@me.com>
2022-07-31 12:04:40 +0200

    Add install script

---
+++ install_gs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Install required packages
+apt update
+apt upgrade
+
+apt install python3-all libpcap-dev libsodium-dev python3-pip python3-pyroute2 python3-future python3-twisted
+apt install virtualenv
+apt install debhelper
+apt install dh-python build-essential
+
+# Create key and copy to right location
+./wfb_keygen
+mv gs.key /etc/gs.key
+
+# Install
+dpkg -i deb_dist/*.deb 
+
+# Setup system
+echo net.core.bpf_jit_enable = 1 >> /etc/sysctl.conf
+
+# Setup config
+cat <<EOT >> /etc/wifibroadcast.cfg
+[common]
+wifi_channel = 161     # 161 -- radio channel @5825 MHz, range: 5815–5835 MHz, width 20MHz
+                       # 1 -- radio channel @2412 Mhz, 
+                       # see https://en.wikipedia.org/wiki/List_of_WLAN_channels for reference
+wifi_region = 'BO'     # Your country for CRDA (use BO or GY if you want max tx power)  
+
+[gs_mavlink]
+peer = 'connect://127.0.0.1:14550'  # outgoing connection
+# peer = 'listen://0.0.0.0:14550'   # incoming connection
+
+[gs_video]
+peer = 'connect://127.0.0.1:5600'  # outgoing connection for
+                                   # video sink (QGroundControl on GS)
+EOT
+
+cat <<EOT >> /etc/default/wifibroadcast
+WFB_NICS="$0"
+EOT
+
+cat <<EOT >> /etc/NetworkManager/NetworkManager.conf
+[keyfile]
+unmanaged-devices=interface-name:$0
+EOT
+
+FILE=/etc/dhcpcd.conf
+if [ -f "$FILE" ]; then
+cat <<EOT >> /etc/dhcpcd.conf
+denyinterfaces $0
+EOT
+fi
+
+# Start gs service
+systemctl daemon-reload
+systemctl start wifibroadcast@gs
