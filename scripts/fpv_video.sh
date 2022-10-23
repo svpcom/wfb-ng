8010ed89e20fe18e5ed1de4a02847e1dd1df6b62
Vasily Evseenko <svpcom@gmail.com>
2022-10-23 13:40:32 +0300

    Add RPI systemd services and scripts

---
+++ scripts/fpv_video.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+set -o pipefail
+gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! 'video/x-h264,stream-format=byte-stream' ! fdsink | fpv_video
+#gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! h264parse  disable-passthrough=true config-interval=1 ! queue ! mmalh264dec ! mmalvideosink sync=false
