e5cefc1171981c917297fb5a75810b874c2808a4
Vasily Evseenko <svpcom@gmail.com>
2023-03-16 22:57:33 +0300

    Fix deb package arch

--- setup.py
+++ setup.py
@@ -3,54 +3,19 @@
 
 import os
 from setuptools import setup, find_packages, command
-import distutils.command.bdist_rpm as orig
-import setuptools.command.bdist_rpm as orig2
 
+# Mark deb package as binary
+try:
+    import stdeb.util
+    class DebianInfo(stdeb.util.DebianInfo):
+        def __init__(self, *args, **kwargs):
+            kwargs['has_ext_modules'] = True
+            super().__init__(*args, **kwargs)
 
-class bdist_rpm(orig.bdist_rpm):
-    """
-    Override the default bdist_rpm behavior to do the following:
+    stdeb.util.DebianInfo = DebianInfo
+except ImportError:
+    pass
 
-    1. Run egg_info to ensure the name and version are properly calculated.
-    2. Always run 'install' using --single-version-externally-managed to
-       disable eggs in RPM distributions.
-    3. Replace dash with underscore in the version numbers for better RPM
-       compatibility.
-    4. Add %global debug_package %{nil} to spec file
-    """
-
-    def run(self):
-        # ensure distro name is up-to-date
-        self.run_command('egg_info')
-
-        orig.bdist_rpm.run(self)
-
-    def _make_spec_file(self):
-        version = self.distribution.get_version()
-        rpmversion = version.replace('-', '_')
-        spec = orig.bdist_rpm._make_spec_file(self)
-        line23 = '%define version ' + version
-        line24 = '%define version ' + rpmversion
-        spec = ['%global debug_package %{nil}'] + \
-               [
-                   line.replace(
-                       "Source0: %{name}-%{version}.tar",
-                       "Source0: %{name}-%{unmangled_version}.tar"
-                ).replace(
-                    "setup.py install ",
-                    "setup.py install --single-version-externally-managed "
-                ).replace(
-                    "%setup",
-                    "%setup -n %{name}-%{unmangled_version}"
-                ).replace(line23, line24)
-                   for line in spec
-               ]
-        insert_loc = spec.index(line24) + 1
-        unmangled_version = "%define unmangled_version " + version
-        spec.insert(insert_loc, unmangled_version)
-        return spec
-
-orig2.bdist_rpm = bdist_rpm
 
 version = os.environ.get('VERSION') or 'trunk'
 commit = os.environ.get('COMMIT')
@@ -58,6 +23,18 @@ commit = os.environ.get('COMMIT')
 if version and commit:
     with open('wfb_ng/conf/site.cfg', 'w') as fd:
         fd.write("# Don't make any changes here, use local.cfg instead!\n\n[common]\nversion = %r\ncommit = %r\n" % (version, commit))
+
+def _long_description():
+    with open('README.md', encoding='utf-8') as fd:
+        start = False
+        for line in fd:
+            if line.startswith('Main features:'):
+                start = True
+            elif line.startswith('#'):
+                break
+
+            if start:
+                yield line
 
 setup(
     url="http://wfb-ng.org",
@@ -79,8 +56,8 @@ setup(
     keywords="wfb-ng, wifibroadcast",
     author="Vasily Evseenko",
     author_email="svpcom@p2ptech.org",
-    description="The next generation of long-range packet radio link based on raw WiFi radio",
-    long_description=open('README.md').read(),
+    description="Long-range packet radio link based on raw WiFi radio",
+    long_description=''.join(_long_description()),
     long_description_content_type='text/markdown',
     license="GPLv3",
 )
