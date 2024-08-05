2d2f452248d4f8156d215db493c70a70288c8f49
Vasily Evseenko <svpcom@gmail.com>
2024-08-06 01:32:49 +0300

    Fix wfb_tx_cmd usage output

--- src/tx_cmd.c
+++ src/tx_cmd.c
@@ -1,3 +1,20 @@
+// Copyright (C) 2024 Vasily Evseenko <svpcom@p2ptech.org>
+
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 3.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -170,6 +187,8 @@ int main(int argc, char **argv)
     if (argc < 3)
     {
         fprintf(stderr, "Usage: %s <port> {set_fec | set_radio} ...\n", argv[0]);
+        fprintf(stderr, "WFB-ng version " WFB_VERSION "\n");
+        fprintf(stderr, "WFB-ng home page: <http://wfb-ng.org>\n");
         return 1;
     }
 
