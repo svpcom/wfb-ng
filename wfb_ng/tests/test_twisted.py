166e66b95f9ea8a6c978c905bcdbaee043ded049
Vasily Evseenko <svpcom@gmail.com>
2024-12-30 23:31:24 +0300

    Add test for twisted timer resolution

--- wfb_ng/tests/test_twisted.py
+++ wfb_ng/tests/test_twisted.py
@@ -1,6 +1,9 @@
 from twisted.trial import unittest
-from twisted.internet import reactor
+from twisted.python import log
+from twisted.internet import reactor, task, defer
 import msgpack
+import time
+import math
 
 class ClockTestCase(unittest.TestCase):
     def test_reactor_has_monitonic_clock(self):
@@ -10,3 +13,24 @@ class ClockTestCase(unittest.TestCase):
         a = ({(1, '2') : (b'spam', 'eggs')},)
         b = msgpack.unpackb(msgpack.packb(a, use_bin_type=True), raw=False, strict_map_key=False, use_list=False)
         self.assertEqual(a, b)
+
+
+    @defer.inlineCallbacks
+    def test_timer_resolution(self):
+        delay = 2e-3
+        iters = 500
+
+        sum = 0
+        sqsum = 0
+
+        for i in range(iters):
+            ts = time.time_ns()
+            yield task.deferLater(reactor, delay)
+            latency = time.time_ns() - ts - delay * 1e9
+            sum += latency
+            sqsum += latency**2
+
+        lat_mean = sum / iters
+        lat_stdmean = 3 * math.sqrt(sqsum / ( iters * (iters - 1)))
+
+        log.msg('Twisted timer resolution: %.2f +- %.2f ms' % (lat_mean / 1e6, lat_stdmean / 1e6))
