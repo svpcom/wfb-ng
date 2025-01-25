2d7fb58372efb4fc329e1e178bbf63cb8eba231a
Vasily Evseenko <svpcom@gmail.com>
2025-01-25 12:08:37 +0300

    Use wifi nics autodetection in install_gs.sh #399

--- scripts/install_gs.sh
+++ scripts/install_gs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Please specify the name of the WiFi adapter'
-    echo 'Find the name using command: ip a'
-    echo 'Aborting ...'
-    exit 1
-fi
+nics="$($(dirname $0)/wfb-nics)"
 
-IFNAME="${1}"
+if [ -z "$nics" ]
+then
+echo "No supported wifi adapters found, please connect them and setup drivers first"
+echo "For 8812au: https://github.com/svpcom/rtl8812au"
+echo "For 8812eu: https://github.com/svpcom/rtl8812eu"
+exit 1
+fi
 
 # Install required packages
 apt update
@@ -20,12 +21,12 @@ apt install python3-all python3-all-dev libpcap-dev libsodium-dev libevent-dev p
 # Build
 make deb
 
+# Install
+apt -y install ./deb_dist/*.deb
+
 # Create key and copy to right location
 ./wfb_keygen
 mv gs.key /etc/gs.key
-
-# Install
-dpkg -i deb_dist/*.deb 
 
 # Setup config
 cat <<EOF >> /etc/wifibroadcast.cfg
@@ -44,12 +45,11 @@ peer = 'connect://127.0.0.1:5600'  # outgoing connection for
                                    # video sink (QGroundControl on GS)
 EOF
 
-echo "WFB_NICS=\"${IFNAME}\"" > /etc/default/wifibroadcast
-
 cat > /etc/modprobe.d/wfb.conf << EOF
 # blacklist stock module
 blacklist 88XXau
 blacklist 8812au
+blacklist 8812
 options cfg80211 ieee80211_regdom=RU
 # maximize output power by default
 #options 88XXau_wfb rtw_tx_pwr_idx_override=30
@@ -59,12 +59,13 @@ options 8812eu rtw_tx_pwr_by_rate=0 rtw_tx_pwr_lmt_enable=0
 EOF
 
 if [ -f /etc/dhcpcd.conf ]; then
-    echo "denyinterfaces ${IFNAME}" >> /etc/dhcpcd.conf
+    echo "denyinterfaces $(nics)" >> /etc/dhcpcd.conf
 fi
 
 # Start gs service
 systemctl daemon-reload
 systemctl start wifibroadcast@gs
-
-echo "Started wifibroadcast@gs"
 systemctl status wifibroadcast@gs
+systemctl enable wifibroadcast@gs
+
+echo "GS setup successfully finished"
