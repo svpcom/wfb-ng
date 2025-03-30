7097c2b797cf19149c6ff6b5a958115cad350dc1
Tipo Man <tipoman9@gmail.com>
2025-03-30 23:47:21 +0300

    Added the option to set WiFi channel by providing frequency im MHz, no need of M adter the freq number

--- wfb_ng/server.py
+++ wfb_ng/server.py
@@ -113,7 +113,7 @@ def init_wlans(max_bw, wlans):
                 channel = settings.common.wifi_channel
 
             if channel>2000:
-                yield call_and_check_rc('iw', 'dev', wlan, 'set', 'freq', str(channel)+ 'M', ht_mode) #set by frequency number in Mhz
+                yield call_and_check_rc('iw', 'dev', wlan, 'set', 'freq', str(channel), ht_mode) #set by frequency number in Mhz
             else:
                 yield call_and_check_rc('iw', 'dev', wlan, 'set', 'channel', str(channel), ht_mode)
 
